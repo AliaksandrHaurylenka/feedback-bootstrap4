46c0f3a04cd8d08ab5717fce69232ae78c513f74
Aliaksandr <aliaksandr.haurylenka@gmail.com>
2018-06-21 06:42:34 +0300

    asd

--- process/SendMail.php
+++ process/SendMail.php
@@ -1,21 +1,21 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: User
  * Date: 07.06.2018
  * Time: 13:00
  */
-
 class SendMail
 {
 
   // функция для проверки количества символов в тексте
   function checkTextLength($text, $minLength, $maxLength)
   {
-    $result=false;
-    $textLength=mb_strlen($text, 'UTF-8');
-    if(($textLength>=$minLength) && ($textLength<=$maxLength)){
-      $result=true;
+    $result = false;
+    $textLength = mb_strlen($text, 'UTF-8');
+    if (($textLength >= $minLength) && ($textLength <= $maxLength)) {
+      $result = true;
     }
     return $result;
   }
@@ -23,48 +23,31 @@ class SendMail
   //фильтрация входящих данных
   function filter($input)
   {
-    $text=filter_var($input, FILTER_SANITIZE_STRING); // защита от XSS
+    $text = filter_var($input, FILTER_SANITIZE_STRING); // защита от XSS
     return $text;
   }
 
-  // валидация формы
-  function val($input, $min, $max, $nameInput)
+
+//валидация форм
+  function length($input, $min, $max)
   {
-    $data['result'] = 'success';
-    $text=$this->filter($input);
-    $checkTextLength=$this->checkTextLength($text, $min, $max);
-    if (isset($input)){
-      if (!$checkTextLength) { // проверка на количество символов в тексте
-        $data[$nameInput] = "Поле <b>".$nameInput."</b> содержит недопустимое количество символов";
-        $data['result'] = 'error';
-      }
-    }
-//    echo json_encode($data);
-//    var_dump($data);
-//    return json_encode($data);
-    return $data;
-  }
-
-
-
-
-    function length($input, $min, $max){
-      $text=$this->filter($input);
-      $checkTextLength=$this->checkTextLength($text, $min, $max);
+    $text = $this->filter($input);
+    $checkTextLength = $this->checkTextLength($text, $min, $max);
 //      var_dump($checkTextLength);
-      return $checkTextLength;
+    return $checkTextLength;
   }
 
-  function res($nameInput){
-    $data[$nameInput] = "Поле <b>".$nameInput."</b> содержит недопустимое количество символов";
-    $data['result'] = 'error';
+  function res($nameInput,  $res, $text)
+  {
+    $data[$nameInput] = "<b>$nameInput</b>".$text;
+    $res;
     return $data;
   }
 
 
   function bodyMail($search, $var, $template)
   {
-    $data=str_replace($search, isset($var) ? $var : '-', $template);
+    $data = str_replace($search, isset($var) ? $var : '-', $template);
     return $data;
   }
 
--- process/process.php
+++ process/process.php
@@ -30,24 +30,22 @@ $email = $send->filter($_POST['email']);
 $message = $send->filter($_POST['message']);*/
 
 
-//проверка на введенное количество символов
-//$send->length($_POST['name'], 2, 10);
-
 //inputы формы для валидации
-/*$send->val($_POST['name'], 2, 100, 'Имя');
-$send->val($_POST['email'], 6, 100, 'E-mail');*/
-/*$data = $send->val($_POST['name'], 2, 100, 'Имя');
-$data = $send->val($_POST['email'], 6, 100, 'E-mail');*/
-//$send->val($_POST['message'], 2, 500, 'Сообщение');
-
-
 $name = $send->length($_POST['name'], 2, 100);
 $email = $send->length($_POST['email'], 6, 100);
-if(!$name){
-  $data = $send->res('Имя');
-}elseif(!$email){
-  $data = $send->res('Email');
-}
+
+
+$text = " содержит недопустимое количество символов";
+/*$result = 'result';
+$res = 'error';*/
+$res['result'] = 'error';
+if(!$name):
+  $data = $send->res('Ф.И.О.', $res, $text);
+//  $data = $send->res('Ф.И.О.', $result, $res, $text);
+elseif(!$email):
+  $data = $send->res('Email', $res, $text);
+//  $data = $send->res('Email', $result, $res, $text);
+endif;
 
 
 
